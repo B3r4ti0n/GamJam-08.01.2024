6078acaceb4360d294f8fd8ec780487f0d73da0e
Mickael-95 <72389929+Mickael-95@users.noreply.github.com>
2024-01-11 00:50:57 +0100

    Add Sound and bear

--- Assets/Scripts/MonsterScript.cs
+++ Assets/Scripts/MonsterScript.cs
@@ -8,16 +8,31 @@ public class MonsterScript : MonoBehaviour
 
     private bool _CanBeActivated;
 
+    [SerializeField]
+    private AudioSource m_AudioSource;
+
+    [SerializeField]
+    private AudioClip m_MonsterAudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        InvokeRepeating("RepeatAudioClip", 0f, 8f);
         _CanBeActivated = true;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void RepeatAudioClip()
+    {
+        m_AudioSource.clip = m_MonsterAudioSource;
+        m_AudioSource.Play();
     }
 
     private void OnCollisionEnter(Collision collision)
